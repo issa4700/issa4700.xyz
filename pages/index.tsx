e63967e0e52aa446974dbf033ccab0aef232c365
issa4700 <issa4700@gmail.com>
2022-01-21 23:57:51 +0000

    Main page redesign to include dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,61 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import styles from "../styles/LinkBtn.module.css";
 
-const socials = [
-  { name: "Linkedin", link: "https://www.linkedin.com/in/issa-abdullah" },
+type LinkBtn = {
+  name: string;
+  label: string;
+};
+
+const links = [
+  { name: "Blog", link: "/writings" },
   { name: "Github", link: "https://github.com/issa4700" },
+  { name: "Linkedin", link: "https://www.linkedin.com/in/issa-abdullah" },
   { name: "Instagram", link: "https://www.instagram.com/issa4700/" },
 ];
 
+function LinkButton({ name, label }: LinkBtn) {
+  return (
+    <Link href={label}>
+      <a className={styles.LinkBtn}>{name}</a>
+    </Link>
+  );
+}
+
 export default function Index() {
   return (
-    <div className="grid place-items-center h-screen">
+    <div className="h-screen bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100">
       <Head>
         <title>Issa Abdullah</title>
         <meta name="description" content="Issa's personal website." />
       </Head>
-
-      <div className="flex flex-col space-y-4 sm:flex-row-reverse items-center ">
-        <figure className="w-full md:w-1/2 relative flex justify-center sm:justify-end ">
-          <img
-            src="/avatar.svg"
-            alt="Profile picture"
-            className="object-cover w-64 sm:h-80 h-64 sm:w-80 rounded-full bg-gray-200"
-          />
-        </figure>
-
-        <div className="w-full md:w-1/2 text-center space-y-4 px-6 sm:text-left">
-          <h1 className="text-5xl font-semibold text-gray-900">
-            Issa Abdullah
-          </h1>
-          <p className="font-thin max-w-prose text-gray-500">
+      <main className="flex flex-col max-w-screen-sm mx-auto p-6 space-y-12 h-full">
+        <header className="space-y-6 mt-8 text-center">
+          <img src="/avatar.svg" className="h-36 w-36 mx-auto rounded-full" />
+          <h1 className="text-4xl font-semibold">Issa Abdullah</h1>
+          <p className="max-w-md mx-auto">
             I am a student at the University of Leeds currently working towards
-            a degree in Aerospace Engineering.
+            a degree in{" "}
+            <span className="text-orange-700 dark:text-orange-300">
+              Aerospace Engineering
+            </span>
+            .
           </p>
-
-          <hr className="sm:hidden" />
-
-          <nav className="text-base flex flex-col sm:mt-6">
-            <Link href="/writings">
-              <a className="text-gray-600 hover:text-gray-900 py-2 sm:py-0 px-2 lg:px-0 hover:underline">
-                Blog
-              </a>
-            </Link>
-
-            {/* Social channels */}
-            {socials.map((social) => (
-              <a
-                className="text-gray-600 hover:text-gray-900 py-2 sm:py-0 px-2 lg:px-0 hover:underline"
-                key={social.name}
-                href={social.link}
-              >
-                {social.name}
-              </a>
-            ))}
-          </nav>
-        </div>
-      </div>
+        </header>
+        <nav className="flex-grow flex flex-col space-y-4">
+          {links.map((social) => (
+            <LinkButton
+              key={social.name}
+              name={social.name}
+              label={social.link}
+            />
+          ))}
+        </nav>
+        <footer className="text-center text-sm text-gray-700 dark:text-gray-200">
+          &copy; Issa Abdullah 2022
+        </footer>
+      </main>
     </div>
   );
 }
