af45eb37f511530bf63c698a518cc08eb6923bc7
issa4700 <issa4700@gmail.com>
2022-07-10 11:41:38 +0100

    Refactored footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import {
   AiOutlineGithub,
   AiOutlineInstagram,
   AiFillLinkedin,
 } from "react-icons/ai";
-
+import { socials } from "settings";
 import Link from "next/link";
 
 const Footer = () => {
@@ -12,7 +11,7 @@ const Footer = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <Link href="/">
             <a>
               <img
@@ -23,26 +22,19 @@ const Footer = () => {
           </Link>
 
           {/* <span className="ml-3 text-xl">issa4700.xyz</span> */}
-        </a>
+        </div>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2022 Issa Abdullah —
-          <a
-            href="https://github.com/issa4700"
-            className="text-gray-600 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            @issa4700
-          </a>
+          <span className="text-gray-600 ml-1">@issa4700</span>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-x-2">
-          <a className="text-gray-500" href="">
+          <a className="text-gray-500" href={socials?.github}>
             <AiOutlineGithub className="h-6 w-6" />
           </a>
-          <a className="text-gray-500" href="">
+          <a className="text-gray-500" href={socials?.instagram}>
             <AiOutlineInstagram className="h-6 w-6" />
           </a>
-          <a className="text-gray-500" href="">
+          <a className="text-gray-500" href={socials?.linkedin}>
             <AiFillLinkedin className="h-6 w-6" />
           </a>
         </span>
---
+++ settings.js
@@ -0,0 +1,5 @@
+export const socials = {
+  github: "https://github.com/issa4700",
+  instagram: "https://www.instagram.com/issa4700/",
+  linkedin: "https://www.linkedin.com/in/issa-abdullah/",
+};
