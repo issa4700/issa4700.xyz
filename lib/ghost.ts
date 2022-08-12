1dbb062170db39131bffc938308068cc9f49b82a
issa4700 <issa4700@gmail.com>
2022-08-12 21:24:00 +0100

    Fix GhostAPI loading wrong environment variables

--- lib/ghost.ts
+++ lib/ghost.ts
@@ -1,8 +1,8 @@
 import GhostContentAPI from "@tryghost/content-api";
 
 const api = new GhostContentAPI({
-  url: process.env.API_URL2 || "",
-  key: process.env.API_KEY2 || "",
+  url: process.env.API_URL || "",
+  key: process.env.API_KEY || "",
   version: "v3",
 });
 
